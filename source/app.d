a3ddd8ae74103eecb0fb0b3402a1a79d5b0d169d
hatf0 <harrison@0xcc.pw>
2021-07-26 22:54:06 -0400

    add project

---
+++ source/app.d
@@ -0,0 +1,13 @@
+import vibe.vibe;
+
+void main()
+{
+	listenHTTP(":8080", &handleRequest);
+	runApplication();
+}
+
+void handleRequest(HTTPServerRequest req, HTTPServerResponse res)
+{
+	if (req.path == "/")
+		res.writeBody("Hello, World!");
+}
